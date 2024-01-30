f443d7795ee7fc8c3f741fb1deb984c3a5b8071e
d4v1d-cub <david.machado@fisica.uh.cu>
2024-01-29 22:22:11 -0500

    Erdos-Renyi graphs added

--- graphs.jl
+++ graphs.jl
@@ -14,7 +14,8 @@ mutable struct HGraph
     nodes_in::Array{Dict{Int64, Int64}, 1}               # dictionary with key=node_index and val=place_in_he   
 end
 
-
+# Builds a hypergraph from the lists of hyperedges per variable (var_2_he),
+# variables per hyperedge (he_2_var) and the list of variable nodes' degrees
 function build_HGraph(var_2_he::Array{Array{Int64, 1}, 1} , he_2_var::Matrix{Int64}, 
                 degrees::Vector{Int64})
     N = length(var_2_he)
@@ -64,12 +65,44 @@ function RRHyperGraph(N::Int64, c::Int64, K::Int64, idum::Int64=1)
 end
 
 
+# Builds a random regular hypergraph with parameters N, c, K and random seed 'idum'
 function build_RR_HGraph(N::Int64, c::Int64, K::Int64, idum::Int64=1)
     var_2_he, he_2_var, degrees = RRHyperGraph(N, c, K, idum)
-    var_2_he = slicematrix(var_2_he)
+    var_2_he = slicematrix(var_2_he)             # The matrix is casted into an array of arrays
     return build_HGraph(var_2_he, he_2_var, degrees)
 end
 
 
-graph = build_RR_HGraph(100, 3, 3)
-graph.degrees
+# This function creates an Erdos-Renyi Hypergaph with mean node connectivity 'c' and factor 
+# node connectivity 'K'. The random seed is controlled by the user
+function ERHyperGraph(N::Int64, c::Int64, K::Int64, idum::Int64=1)
+    Random.seed!(idum)
+    M = N * c / K 
+    if isinteger(M)
+        M = Int64(M)
+        he_2_var = zeros(Int64, (M, K))
+        var_2_he = fill(Array{Int64, 1}(), N)
+        degrees = zeros(Int64, N)
+        for he in 1:M
+            for i in 1:K
+                node = rand(1:N)
+                he_2_var[he, i] = node
+                push!(var_2_he[node], he)
+                degrees[node] += 1
+                # Each hyperedge is formed by 'K' variables chosen uniformly at random
+                # This leads to a Poisson distribution of conectivities (ER Graph) 
+            end
+        end
+        return var_2_he, he_2_var, degrees  
+    else
+        println("The number of factor nodes 'N * c / K' must be an integer")
+        return nothing
+    end      
+end
+
+
+# Builds a random regular hypergraph with parameters N, c, K and random seed 'idum'
+function build_ER_HGraph(N::Int64, c::Int64, K::Int64, idum::Int64=1)
+    var_2_he, he_2_var, degrees = ERHyperGraph(N, c, K, idum)
+    return build_HGraph(var_2_he, he_2_var, degrees)
+end
