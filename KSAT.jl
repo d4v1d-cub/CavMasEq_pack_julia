3cebc9232380d1b34c4306f25fa9d03db7116866
David Machado <davidmachadop@gmail.com>
2024-02-18 21:29:45 -0500

    added der pcav single

--- KSAT.jl
+++ KSAT.jl
@@ -123,7 +123,11 @@ function get_pu_lpm(node::Int64, he_list::Vector{Int64}, nodes_in::Vector{Dict{I
                     pu::Matrix{Float64})
     places_in = map(x -> get(x, node, "Error: Node not found in he"), nodes_in[he_list])
     iter = zip(he_list, places_in)
-    return [pu[i, j] for (i, j) in iter]
+    pu_lpm = Vector{Float64}()
+    for (i, j) in iter
+        push!(pu_lpm, pu[i, j])
+    end
+    return pu_lpm
 end
 
 
@@ -142,20 +146,6 @@ function construct_pu_neighs(node::Int64, he::Int64, pu::Matrix{Float64},
     pu_lm = get_pu_lpm(node, he_list, nodes_in, pu)
     return pu_lp, pu_lm
 end
-
-
-# n = 100
-# c = 3
-# K = 3
-# g1 = build_ER_HGraph(n, c, K)
-# all_l = gen_links(g1)
-# all_lp, all_lm = all_lpm(g1, all_l)
-
-# ch_u_cond = unsat_ch(g1, all_l)
-# p_cav = init_p_cav_KSAT(g1, 0.5)
-# pu = comp_pu_KSAT(p_cav, g1, ch_u_cond)
-
-# construct_pu_neighs(g1.he_2_var[1, 1], 1, pu, g1.nodes_in, all_lp, all_lm)
 
 
 # This function recursively computes a vector fE[k], with k=1,..., c+1
@@ -177,33 +167,127 @@ end
 
 
 # This function computes the sum-product in the CME for K-SAT.
-# The rates depend on the energy for the current value of the variable (Ep)
-# and on the energy after flipping the variable (Em)
-function sum_product_KSAT(pu_lp::Vector{Float64}, pu_lm::Vector{Float64}, ratefunc::Function, 
-                          rate_args::Vector{Any}, Eap::Int8, Eam::Int8)
-    cp = length(pu_lp)
-    cm = length(pu_lm)
-    fEp = recursive_marginal(pu_lp, cp, 1, [1.0])
-    fEm = recursive_marginal(pu_lm, cm, 1, [1.0])
-    s = 0.0
-    for Ep in 0:cp
-        for Em in 0:cm
-            s += ratefunc(Ep + Eap, Em + Eam, rate_args...) * fEp[Ep] * fEm[Em]
+# The rates depend on the energy for the current value of the variable (Eu)
+# and on the energy after flipping the variable (Es)
+# pu_lu are the probabilities for unsatisfied links
+# pu_ls are the probabilities for satisfied links
+# Ea is the state of the origin clause
+# Ea_flip is the state when the spin flips
+function sum_product_KSAT(pu_lu::Vector{Float64}, pu_ls::Vector{Float64}, ratefunc::Function, 
+                          rate_args, Ea::Int, Ea_flip::Int)
+    cu = length(pu_lu)
+    cs = length(pu_ls)
+    fE_u = recursive_marginal(pu_lu, cu, 1, [1.0])  # This corresponds to links
+    fE_s = recursive_marginal(pu_ls, cs, 1, [1.0])
+    cumul = 0.0
+    for Eu in 0:cu
+        for Es in 0:cs
+            cumul += ratefunc(Eu + Ea, Es + Ea_flip, rate_args...) * fE_u[Eu + 1] * fE_s[Es + 1]
         end
     end
-    return s
+    return cumul
 end
 
 
 # This function computes the sum-product in the CME for K-SAT.
-# The rates depend only on the energy for the current value of the variable (Ep)
-function sum_product_KSAT(pu_lp::Vector{Float64}, ratefunc::Function, 
-                          rate_args::Vector{Any}, Eap::Int8)
-    cp = length(pu_lp)
-    fEp = recursive_marginal(pu_lp, cp, 1, [1.0])
-    s = 0.0
-    for Ep in 0:cp
-        s += ratefunc(Ep + Eap, rate_args...) * fEp[Ep]
+# The rates depend only on the energy for the current value of the variable (Eu)
+function sum_product_KSAT(pu_lu::Vector{Float64}, ratefunc::Function, 
+                          rate_args, Ea::Int)
+    cu = length(pu_lu)
+    fE_u = recursive_marginal(pu_lu, cu, 1, [1.0])
+    cumul = 0.0
+    for Eu in 0:cu
+        cumul += ratefunc(Eu + Ea, rate_args...) * fE_u[Eu]
     end
-    return s
+    return cumul
 end
+
+
+# This function gives all sums needed in the computation of the derivative of the pcav
+# related to he and node.
+# It returns an array all_sums[val, Ea, Ea_flip]
+# When val=1 (si = 1) the array of probabilities pu_lu is taken from the negative links
+# because those are the unsatisfied links. At the same time, pu_ls is taken from the positive
+# links, which will become unsatisfied when si flips
+# When val=2 the roles are inverted.
+# Ea is the value of the clause 'a' inside pcav
+# Ea_flip is the value when si flips
+function compute_all_sums(pu::Matrix{Float64},
+                    node::Int64, he::Int64, graph::HGraph, 
+                    all_lp::Vector{Vector{Vector{Int64}}}, 
+                    all_lm::Vector{Vector{Vector{Int64}}}, ratefunc::Function, 
+                    rate_args)
+    all_sums = zeros(Float64, (2, 2, 2))
+    for val in 1:2
+        if val == 1
+            pu_lu, pu_ls = construct_pu_neighs(node, he, pu, graph.nodes_in, all_lm, all_lp)
+        else
+            pu_lu, pu_ls = construct_pu_neighs(node, he, pu, graph.nodes_in, all_lp, all_lm)
+        end
+        all_sums[val, 1, 1] = sum_product_KSAT(pu_lu, pu_ls, ratefunc, rate_args, 0, 0)
+        all_sums[val, 2, 1] = sum_product_KSAT(pu_lu, pu_ls, ratefunc, rate_args, 1, 0)
+        all_sums[val, 1, 2] = sum_product_KSAT(pu_lu, pu_ls, ratefunc, rate_args, 0, 1)
+        # The triad (val, 1, 1) it is not possible.
+    end    
+    
+    return all_sums
+end
+
+
+# This function computes one term in the derivative of pcav. It corresponds to
+# the flipping of the variable indexed as 'index_in' among the variables in the argument
+# of p_cav. The array 'all_sums' is computed using the function 'compute_all_sums'
+# ch_exc is an integer that codes the combination of the variables in the argument of the
+# probability 'p_cav'. Remember that all p_cav are conditioned to one already unsatisfied link.
+function der_pcav_node_KSAT(p_cav::Vector{Float64}, all_sums::Array{Float64, 3}, 
+                            ch_exc::Int64, ch_exc_unsat::Int64, 
+                            index_in::Int64)
+    ch_exc_flip = (ch_exc ⊻ (2 ^ (index_in - 1)))  # The ⊻ (xor) operation flips the variable
+    val = ((ch_exc >> (index_in - 1)) & 1)         # Takes the value of the variable
+    Ea = (ch_exc == ch_exc_unsat)                  # As the variable in the conditional is set 
+    Ea_flip = (ch_exc_flip == ch_exc_unsat)        # to always unsatisfy its link, the clause
+    # will be unsatisfied only if the rest of the variables are in their unsatisfied configuration
+    
+    return -all_sums[val + 1, Ea + 1, Ea_flip + 1] * p_cav[ch_exc + 1] + 
+            all_sums[2 - val, Ea_flip + 1, Ea + 1] * p_cav[ch_exc_flip + 1]
+end
+
+
+# This function computes the derivatives of all the p_cav that correspond to a clause
+function der_pcav_KSAT(p_cav::Matrix{Float64}, pu::Matrix{Float64}, he::Int64, graph::HGraph, 
+    all_lp::Vector{Vector{Vector{Int64}}}, 
+    all_lm::Vector{Vector{Vector{Int64}}}, ratefunc::Function, 
+    rate_args, links::Matrix{Int8})
+    
+    nch = graph.chains_he ÷ 2
+    ders = zeros(Float64, (graph.K, nch))
+    for i in 1:graph.K
+        lvec_rev = 1 .- links[he, 1:end .!= i]  # gives the configuration that unsatisfies every link
+        ch_exc_unsat = digits2int(lvec_rev)    # converting it to an integer
+        for j in 1:graph.K-1
+            node = graph.nodes_except[he,i,j]
+            all_sums = compute_all_sums(pu, node, he, graph, all_lp, all_lm, ratefunc, rate_args)
+            for ch_exc in 0:nch-1
+                ders[i, ch_exc + 1] += der_pcav_node_KSAT(p_cav[i, :], all_sums, ch_exc, 
+                                                         ch_exc_unsat, j)   
+            end
+        end
+    end
+    return ders
+end
+
+n = 100
+c = 3
+K = 3
+g1 = build_ER_HGraph(n, c, K)
+all_l = gen_links(g1)
+all_lp, all_lm = all_lpm(g1, all_l)
+
+ch_u_cond = unsat_ch(g1, all_l)
+p_cav = init_p_cav_KSAT(g1, 0.5)
+pu = comp_pu_KSAT(p_cav, g1, ch_u_cond)
+g1.K
+
+include("rates.jl")
+der_pcav_KSAT(p_cav[1, :, :], pu, 1, g1, all_lp, all_lm, rate_FMS_KSAT, [1.0, 1.0, g1.K], all_l)
+
--- rates.jl
+++ rates.jl
@@ -1,7 +1,7 @@
 include("graphs.jl")
 
 # This is the rate of FMS algorithm for KSAT
-function rate_FMS_KSAT(Ep::Int64, Em::Int64, T::Float64, avE::Float64, K::Int64)
+function rate_FMS_KSAT(Ep::Int64, Em::Int64, T::Float64, avE::Float64, K::Number)
     dE = Em - Ep
     if dE > 0
         return Ep / K / avE * exp(-dE / T)
