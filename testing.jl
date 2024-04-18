aeafe9eec465ebb1d131da58c18e4a8a218113db
David Machado <davidmachadop@gmail.com>
2024-04-18 13:55:18 -0400

    Seeds must be different

--- testing.jl
+++ testing.jl
@@ -4,22 +4,29 @@ using OrdinaryDiffEq, DiffEqCallbacks
 
 println("Packages loaded")
 
+# using Random
+# include("./general.jl")
+# include("./graphs.jl")
+# include("./rates.jl")
+# include("./KSAT.jl")
+# include("./CME.jl")
+
 # n = parse(Int64, ARGS[1])
 # alpha = parse(Float64, ARGS[2])
-n = 1000
-alpha = 2.8
+N = 100
+alpha = 2.3
 K = 3
 c = K * alpha
 p0 = 0.5
 seed = 1
 
-eta = 0.8
+eta = 1.0
 alg_str = "FMS"
 rf = rate_FMS_KSAT
 rargs = [eta]
 
 t0 = 0.0
-tlim = 1
+tlim = 10
 
 
 saved_eners_CME = SavedValues(Float64, Float64)
@@ -29,21 +36,52 @@ cbs_save_CME = CallbackSet(cb_ener_CME)
 tspan = [t0, tlim]
 
 println("Running integration")
-answ = CME_KSAT(rf, rargs, build_args_rate_FMS, N=n, K=K, alpha=alpha, 
-                seed_g=seed, seed_l=seed, tspan=[t0, tlim], cbs_save=cbs_save_CME)
+answ = CME_KSAT(rf, rargs, build_args_rate_FMS, N=N, K=K, alpha=alpha, 
+                seed_g=seed, seed_l=seed+1, tspan=[t0, tlim], cbs_save=cbs_save_CME)
 
 saved_eners_CDA = SavedValues(Float64, Float64)
 cb_ener_CDA = SavingCallback(save_ener_CDA, saved_eners_CDA)
 cbs_save_CDA = CallbackSet(cb_ener_CDA)
 
-answ = CDA_KSAT(rf, rargs, build_args_rate_FMS, N=n, K=K, alpha=alpha, 
-                seed_g=seed, seed_l=seed, tspan=[t0, tlim], cbs_save=cbs_save_CDA)
+answ = CDA_KSAT(rf, rargs, build_args_rate_FMS, N=N, K=K, alpha=alpha, 
+                seed_g=seed, seed_l=seed+1, tspan=[t0, tlim], cbs_save=cbs_save_CDA)
 
 
-fileener = "CME_KSAT_" * alg_str * "_K_" * string(K) * "_N_" * string(n) * "_alpha_" * 
-           string(alpha) * "_p0_" * string(p0) * "_eta_" * string(eta) * "_t0_" * string(t0) * 
-           "_tmax_" * string(tlim) * ".txt"
+# fileener = "CME_KSAT_" * alg_str * "_K_" * string(K) * "_N_" * string(n) * "_alpha_" * 
+#            string(alpha) * "_p0_" * string(p0) * "_eta_" * string(eta) * "_t0_" * string(t0) * 
+#            "_tmax_" * string(tlim) * ".txt"
 
-print_ener(saved_eners, fileener)
+# print_ener(saved_eners_CME, fileener)
 
-println("Integration finished")
+# println("Integration finished")
+
+
+
+# efinal = 1e-4 * N
+# graph = build_ER_HGraph(N, c, K, 1)
+# links = gen_links(graph, 2)
+
+# node = 10
+# all_index = map(x -> get(x, node, "Error"), graph.nodes_in[graph.var_2_he[node]])
+
+# for i in eachindex(all_index)
+#     print(links[graph.var_2_he[node][i], all_index[i]])
+# end
+
+# all_lp, all_lm = all_lpm(graph, links)
+# ch_u, ch_u_cond = unsat_ch(graph, links)
+# p_cav = init_p_cav(graph, p0)
+# probi = fill(p0, graph.N)
+# len_cav = length(p_cav)
+
+# params = graph, all_lp, all_lm, links, ch_u, ch_u_cond, rf, rargs, build_args_rate_FMS, len_cav, 
+#          efinal
+
+# u = vcat(reshape(p_cav, len_cav), probi)
+# du = zeros(Float64, size(u))
+
+# fder_KSAT_CME(du, u, params, 0.0)
+
+# u .+= du * 0.01
+
+# fder_KSAT_CME(du, u, params, 0.0)
