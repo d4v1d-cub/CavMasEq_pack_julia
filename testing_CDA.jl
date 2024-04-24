c1b54b950014c4b5bc5e1f23040c3255503e27ee
David Machado <davidmachadop@gmail.com>
2024-04-23 22:56:15 -0400

    for cluster

--- testing_CDA.jl
+++ testing_CDA.jl
@@ -2,24 +2,23 @@ include("./ApproxMasEq.jl")
 using .ApproxMasEq
 using OrdinaryDiffEq, DiffEqCallbacks
 
-# println("Packages loaded")
-
 N = parse(Int64, ARGS[1])
 alpha = parse(Float64, ARGS[2])
-# N = 1000
-# alpha = 2.5
-K = 3
+K = parse(Int64, ARGS[3])
 c = K * alpha
-p0 = 0.5
-seed = 1
+p0 = parse(Float64, ARGS[4])
+seed = parse(Int64, ARGS[5])
 
-eta = 1.0
+eta = parse(Float64, ARGS[6])
 alg_str = "FMS"
 rf = rate_FMS_KSAT
 rargs = [eta]
 
 t0 = 0.0
-tlim = 10
+tlim = parse(Float64, ARGS[7])
+
+# N = 1000
+# alpha = 2.5
 
 saved_eners_CDA = SavedValues(Float64, Float64)
 cb_ener_CDA = SavingCallback(save_ener_CDA, saved_eners_CDA)
@@ -27,7 +26,6 @@ cbs_save_CDA = CallbackSet(cb_ener_CDA)
 
 tspan = [t0, tlim]
 
-# println("Running integration")
 answ = CDA_KSAT(rf, rargs, build_args_rate_FMS, N=N, K=K, alpha=alpha, 
                 seed_g=seed, seed_l=seed, tspan=[t0, tlim], cbs_save=cbs_save_CDA, dt_s=1.0)
 
@@ -37,5 +35,3 @@ fileener = "../Test/CDA_KSAT_" * alg_str * "_K_" * string(K) * "_N_" * string(N)
            "_tmax_" * string(tlim) * ".txt"
 
 print_ener(saved_eners_CDA, fileener)
-
-# println("Integration finished")
--- testing_CME.jl
+++ testing_CME.jl
@@ -2,31 +2,23 @@ include("./ApproxMasEq.jl")
 using .ApproxMasEq
 using OrdinaryDiffEq, DiffEqCallbacks
 
-# println("Packages loaded")
-
-# using Random
-# include("./general.jl")
-# include("./graphs.jl")
-# include("./rates.jl")
-# include("./KSAT.jl")
-# include("./CME.jl")
-
 N = parse(Int64, ARGS[1])
 alpha = parse(Float64, ARGS[2])
-# N = 1000
-# alpha = 2.2
-K = 3
+K = parse(Int64, ARGS[3])
 c = K * alpha
-p0 = 0.5
-seed = 1
+p0 = parse(Float64, ARGS[4])
+seed = parse(Int64, ARGS[5])
 
-eta = 1.0
+eta = parse(Float64, ARGS[6])
 alg_str = "FMS"
 rf = rate_FMS_KSAT
 rargs = [eta]
 
 t0 = 0.0
-tlim = 10
+tlim = parse(Float64, ARGS[7])
+
+# N = 1000
+# alpha = 2.5
 
 
 saved_eners_CME = SavedValues(Float64, Float64)
@@ -35,7 +27,6 @@ cbs_save_CME = CallbackSet(cb_ener_CME)
 
 tspan = [t0, tlim]
 
-# println("Running integration")
 answ = CME_KSAT(rf, rargs, build_args_rate_FMS, N=N, K=K, alpha=alpha, 
                 seed_g=seed, seed_l=seed, tspan=[t0, tlim], cbs_save=cbs_save_CME, dt_s=2.5)
 
@@ -45,36 +36,3 @@ fileener = "../Test/CME_KSAT_" * alg_str * "_K_" * string(K) * "_N_" * string(N)
            "_tmax_" * string(tlim) * ".txt"
 
 print_ener(saved_eners_CME, fileener)
-
-# println("Integration finished")
-
-# efinal = 1e-4 * N
-# graph = build_RR_HGraph(N, c, K, 1)
-# links = gen_links(graph, 1)
-
-# all_lp, all_lm = all_lpm(graph, links)
-# ch_u, ch_u_cond = unsat_ch(graph, links)
-# p_cav = init_p_cav(graph, p0)
-# probi = fill(p0, graph.N)
-# len_cav = length(p_cav)
-
-# params = graph, all_lp, all_lm, links, ch_u, ch_u_cond, rf, rargs, build_args_rate_FMS, len_cav, 
-#          efinal
-
-# u = vcat(reshape(p_cav, len_cav), probi)
-# du = zeros(Float64, size(u))
-
-# fder_KSAT_CME(du, u, params, 0.0)
-
-# u .+= du * 0.01
-
-# fder_KSAT_CME(du, u, params, 0.0)
-
-
-
-# node = 8
-# all_index = map(x -> get(x, node, "Error"), graph.nodes_in[graph.var_2_he[node]])
-
-# for i in eachindex(all_index)
-#     print(links[graph.var_2_he[node][i], all_index[i]])
-# end
